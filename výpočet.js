e75274a021fd05b00a39f19f180476e54f3ef256
peroutkovap <peroutkovap@spsstav.cz>
2023-04-24 11:31:02 +0200

    oprava

--- výpočet.js
+++ výpočet.js
@@ -1,10 +1,27 @@
 function mocnina(cislo, exponent) {
   return Math.pow(cislo, exponent);
-}  
-function vypocet(){
-const cislo=document.getElementById("strana_a").value
-const exponent=3
-const vysledek= mocnina(cislo,exponent);
-console.log(vysledek);
-document.getElementById("vysledek").innerText=vysledek
-} 
+}
+function vypocet() {
+  const cislo = document.getElementById("strana_a").value
+  const exponent = 3
+  const vysledek = mocnina(cislo, exponent);
+  console.log(vysledek);
+  document.getElementById("vysledek").innerText = vysledek
+}
+function ukazatvysledek() {
+  const cislo = document.getElementById("strana_a").value
+  const exponent = 3
+  const vysledek = mocnina(cislo, exponent);
+  const hodnota = document.getElementById("hodnota").value
+  if (hodnota == vysledek) {
+    document.getElementById("vysledek2").innerText = "správně"
+  } else {
+    document.getElementById("vysledek2").innerText = "špatně"
+  }
+  document.getElementById("vysledek").innerText = vysledek;
+}
+
+
+function spravnyvysledek() {
+  document.getElementById("vysledek").innerText = vysledek
+};
